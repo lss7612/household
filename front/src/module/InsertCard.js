3295008f4b9c1566c85b4033a1863a84b7406f67
sangsung-lee <sslee@kr.doubledown.com>
2022-03-03 10:00:19 +0900

    220303 - card css ing

---
+++ front/src/module/InsertCard.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./css/InsertCard.css";
+function InsertCard() {
+  //TOTO  카테고르 기져오기 필요함
+  const [category, setCategory] = useState(new Array());
+  const [isCard, setIsCard] = useState();
+
+  return (
+    <div className="card">
+      <div className="input_area">
+        <label htmlFor="spend">금액</label>
+        <input type="number" id="spend" />
+      </div>
+      {isCard ? <CardInput /> : null}
+      <div className="input_area">
+        <label htmlFor="category">분류</label>
+        <select></select>
+        <datalist></datalist>
+      </div>
+      <div className="input_area">
+        <label htmlFor="comment">코멘트</label>
+        <input />
+      </div>
+    </div>
+  );
+}
+
+function CardInput() {
+  return (
+    <div className="input_area">
+      <label htmlFor="spend">카드</label>
+      <select></select>
+    </div>
+  );
+}
+
+export default InsertCard;
--- front/src/routes/Insert.js
+++ front/src/routes/Insert.js
@@ -1,7 +1,16 @@
+import InsertCard from "../module/InsertCard";
+
 function Insert() {
   return (
     <div className="article">
       <h1>Insert</h1>
+      <ul>
+        <li>금액 insert</li>
+        <li>카드 or 현금 >> 카드면 무슨카드?</li>
+        <li>대분류</li>
+        <li>코멘트</li>
+      </ul>
+      <InsertCard />
     </div>
   );
 }
